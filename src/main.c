bc12f970f91c5be06b349848157b34e0a1a4375f
Albin Wahlén <alegow8@gmail.com>
2022-07-28 17:36:10 +0200

    RTC test

--- src/main.c
+++ src/main.c
@@ -31,15 +31,36 @@ int main(void){
     // Write to display
     log(INFO, "Start test program\n");
 
-    set_display_buffer_string("TEST", 0);
-    print_all_display_buffers();
+    set_display_buffer_long_string("TEST RTC", 8);
     write_to_all_displays();
 
     sleep_ms1(1000);
 
-    set_display_buffer_long_string("Albin W", 7);
+    log(INFO, "Test RTC\n");
+    uint8_t config[] = {0x80};
+    TWI_write_bytes(0b1101111, 0, config, 1);
 
-    write_to_all_displays();
+    while (1) {
+        busy_wait_ms1(1000);
+        TWI_read(0b1101111, 0, 3);
+        TWI_wait();
+        char s0 = (0x0f & twi_data[0]) + 0x30;
+        char s1 = ((0x70 & twi_data[0])>>4) + 0x30;
+        char m0 = (0x0f & twi_data[1]) + 0x30;
+        char m1 = ((0x70 & twi_data[1])>>4) + 0x30;
+        char h0 = (0x0f & twi_data[2]) + 0x30;
+        char h1 = ((0x70 & twi_data[2])>>4) + 0x30;
+        clear_all_display_buffers();
+        add_char_to_display_buffer(s0, 3, 1);
+        add_char_to_display_buffer(s1, 2, 1);
+        add_char_to_display_buffer(m0, 1, 1);
+        add_char_to_display_buffer(m1, 0, 1);
+        add_colon_to_display_buffer(1);
+        add_char_to_display_buffer(h0, 3, 0);
+        add_char_to_display_buffer(h1, 2, 0);
+
+        write_to_all_displays();
+    }
 
     log(INFO, "Test program finished\n");
     while(1);
