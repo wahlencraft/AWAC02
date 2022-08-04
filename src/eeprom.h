d2d0a068e802e198579d028e0edfb91cbf17483d
Albin Wahlén <alegow8@gmail.com>
2022-08-04 19:32:24 +0200

    Comments in eeprom.h

--- src/eeprom.h
+++ src/eeprom.h
@@ -1,8 +1,18 @@
+/* This is a header declaring the functions defined by assembly rutines in
+ * eeprom.S.
+ *
+ * Theese functions allows reads and writes to non-volatile memory. So the data
+ * will remain even if power is lost. */
+
 #ifndef EEPROM_H_
 #define EEPROM_H_
 
+/* Write one byte to EEPROM.
+ *
+ * Note, this is very slow (typicaly 3.3 ms). */
 extern void EEPROM_write(uint16_t address, uint8_t data);
 
+/* Write one byte to EEPROM. */
 extern uint8_t EEPROM_read(uint16_t address);
 
 #endif  // EEPROM_H_
